1bbf33a394c7105561019439323b9a53e2807571
Saikumar <muthyalasaikumar9@gmail.com>
2022-05-02 08:59:21 +0530

    fifth comment

--- ucemployee/Program.cs
+++ ucemployee/Program.cs
@@ -10,29 +10,31 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            int Is_Part_Time = 2;
-            int Is_Full_Time = 1;
             int Wage_Per_Hr = 20;
-            int Daily_Wage = 0;
-            int WorkingHrs = 0;
-            Random r = new Random();
-            int check = r.Next(0, 3);
-            switch (check)
+            int Daily_Wage = 0, Count_WorkingHrs = 0;
+            int WorkingHrs = 0, workingDays = 20, days;
+            for (days = 0; days <= workingDays; days++)
             {
-                case 1:
-                    WorkingHrs = 8;
-                    break;
-                case 2:
-                    WorkingHrs = 4;
-                    break;
-                default:
-                    WorkingHrs = 0;
-                    break;
+
+                Random r = new Random();
+                int check = r.Next(0, 3);
+                switch (check)
+                {
+                    case 1:
+                        WorkingHrs = 8;
+                        break;
+                    case 2:
+                        WorkingHrs = 4;
+                        break;
+                    default:
+                        WorkingHrs = 0;
+                        break;
+                }
+                Count_WorkingHrs = Count_WorkingHrs + WorkingHrs;
             }
-            Daily_Wage = WorkingHrs * Wage_Per_Hr;
-            Console.WriteLine(Daily_Wage);
+            Daily_Wage = Count_WorkingHrs * Wage_Per_Hr;
+            Console.WriteLine("salary : " + Daily_Wage);
             Console.ReadLine();
         }
     }
 }
-© 2022 GitHub, Inc.
