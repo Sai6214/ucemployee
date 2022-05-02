8c22b2e9e8ee5b7da96d280e3d5ea629bcefa0d0
Saikumar <muthyalasaikumar9@gmail.com>
2022-05-02 08:52:26 +0530

    third commit

--- ucemployee/Program.cs
+++ ucemployee/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EmployeeWage
 {
@@ -6,14 +10,17 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            int Is_Present = 1;
+            int Is_Part_Time = 2;
+            int Is_Full_Time = 1;
             int Wage_Per_Hr = 20;
             int Daily_Wage = 0;
             int WorkingHrs = 0;
             Random r = new Random();
-            int check = r.Next(0, 2);
-            if (Is_Present == check)
+            int check = r.Next(0, 3);
+            if (Is_Full_Time == check)
                 WorkingHrs = 8;
+            if (Is_Part_Time == check)
+                WorkingHrs = 4;
             else
                 WorkingHrs = 0;
             Daily_Wage = WorkingHrs + Wage_Per_Hr;
